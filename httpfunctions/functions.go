f1b276892239c4486a5c0b402ea207587757e909
pavlus01 <egopaw01@gmail.com>
2023-05-09 11:32:25 +0200

    zajecia 9 maja

--- httpfunctions/functions.go
+++ httpfunctions/functions.go
@@ -1,23 +1,16 @@
 package httpfunctions
 
 import (
-	//     "encoding/json"
-	//     "fmt"
-	//     "log"
 	"bytes"
-	"net/http"
-
-	// "net/url"
-
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	gui "github.com/grupawp/warships-gui/v2"
-	// color "github.com/fatih/color"
-	// gui "github.com/grupawp/warships-lightgui"
 )
 
 type Request_data struct {
@@ -37,6 +30,13 @@ type StatusResponse struct {
 	Opponent         string   `json:"opponent"`
 	Should_fire      bool     `json:"should_fire"`
 	Timer            int      `json:"timer"`
+}
+
+type GameDescription struct {
+	Desc     string `json:"desc"`
+	Nick     string `json:"nick"`
+	Opp_desc string `json:"opp_desc"`
+	Opponent string `json:"opponent"`
 }
 
 type GameStatus struct {
@@ -64,14 +64,12 @@ type Fire_result struct {
 func FirstConnection(desc, name string) error {
 	request_data := Request_data{Desc: desc, Nick: name, Target_nick: "", Wpbot: true}
 	encoded_data, _ := json.Marshal(request_data)
-	// log.Print(string(encoded_data))
 
 	resp, err := http.Post("https://go-pjatk-server.fly.dev/api/game", "application/json", bytes.NewBuffer(encoded_data))
 	if err != nil {
 		return fmt.Errorf("cannot create request: %w", err)
 	}
 	token := resp.Header.Get("x-auth-token")
-	// log.Print("Token " + token)
 
 	time.Sleep(time.Second * 4)
 	client := &http.Client{Timeout: time.Second * 5}
@@ -83,19 +81,11 @@ func FirstConnection(desc, name string) error {
 	}
 
 	var data GameStatus
-	// resBody, err := ioutil.ReadAll(resp2.Body)
-	// if err != nil {
-	// 	fmt.Printf("client: could not read response body: %s\n", err)
-	// 	os.Exit(1)
-	// }
-	// log.Print(string(resBody))
+
 	err = json.NewDecoder(resp2.Body).Decode(&data)
 	if err != nil {
 		return fmt.Errorf("cannot unmarshal data: %w", err)
 	}
-	// log.Print("Game Status " + data.Game_status)
-	// log.Print("YOUR NICK " + data.Nick)
-	// log.Print(data)
 
 	req2, err4 := http.NewRequest("GET", "https://go-pjatk-server.fly.dev/api/game/board", nil)
 	req2.Header.Add("x-auth-token", token)
@@ -112,7 +102,20 @@ func FirstConnection(desc, name string) error {
 		return fmt.Errorf("cannot unmarshal data: %w", err)
 	}
 
-	// log.Print(bo)
+	req3, err6 := http.NewRequest("GET", "https://go-pjatk-server.fly.dev/api/game/desc", nil)
+	req3.Header.Add("x-auth-token", token)
+	if err6 != nil {
+		return fmt.Errorf("cannot unmarshal data: %w", err)
+	}
+	resp5, err7 := client.Do(req3)
+	if err7 != nil {
+		return fmt.Errorf("cannot unmarshal data: %w", err)
+	}
+	var gDesc GameDescription
+	err = json.NewDecoder(resp5.Body).Decode(&gDesc)
+	if err != nil {
+		return fmt.Errorf("cannot unmarshal data: %w", err)
+	}
 
 	ui := gui.NewGUI(true)
 
@@ -120,6 +123,17 @@ func FirstConnection(desc, name string) error {
 	ui.Draw(txt)
 	ui.Draw(gui.NewText(1, 2, "Press Ctrl+C to exit", nil))
 	ui.Draw(gui.NewText(1, 3, data.Nick+" vs "+data.Opponent, nil))
+	if len(gDesc.Desc) > 90 {
+		ui.Draw(gui.NewText(1, 27, gDesc.Desc[:30], nil))
+		ui.Draw(gui.NewText(1, 28, gDesc.Desc[30:60], nil))
+		ui.Draw(gui.NewText(1, 29, gDesc.Desc[60:90], nil))
+	}
+	ui.Draw(gui.NewText(1, 27, gDesc.Desc[:30], nil))
+	ui.Draw(gui.NewText(1, 28, gDesc.Desc[30:60], nil))
+	ui.Draw(gui.NewText(1, 29, gDesc.Desc[60:90], nil))
+	ui.Draw(gui.NewText(50, 27, gDesc.Opp_desc[:30], nil))
+	ui.Draw(gui.NewText(50, 28, gDesc.Opp_desc[30:60], nil))
+	ui.Draw(gui.NewText(50, 29, gDesc.Opp_desc[60:90], nil))
 
 	// boardConfig := gui.BoardConfig{RulerColor: gui.Color{Red: 236, Green: 54, Blue: 54}, TextColor: gui.Color{Red: 88, Green: 243, Blue: 212}}
 	board := gui.NewBoard(1, 5, nil)
@@ -141,68 +155,55 @@ func FirstConnection(desc, name string) error {
 	ui.Log("FIRE: %s", data)
 	myHitCounter := 0
 	oppHitCounter := 0
+	coordsChecked := 0
 
 	if len(data.Opp_shots) != 0 {
 
 		ui.Log("DATA: %s", data)
-		x, y := ChangeCooerdinate(data.Opp_shots[len(data.Opp_shots)-1])
-		if states[x][y-1] == gui.Ship {
-			oppHitCounter++
-			states[x][y-1] = gui.Hit
-			time.Sleep(time.Second * 1)
-			Get_Opp_Shot(board, board2, &states, &states2, ui, client, token, &oppHitCounter)
-			if oppHitCounter == 20 {
-				txt.SetText(fmt.Sprintf("OPPONENT WON: %s", data.Opponent))
+		shotsCounter := 0
+		for i := coordsChecked; i < len(data.Opp_shots); i++ {
+			x, y := ChangeCooerdinate(data.Opp_shots[i])
+			shotsCounter++
+			if states[x][y-1] == gui.Ship {
+				oppHitCounter++
+				states[x][y-1] = gui.Hit
+				time.Sleep(time.Second * 1)
+				Get_Opp_Shot(board, board2, &states, &states2, ui, client, token, &oppHitCounter, &coordsChecked)
+			} else {
+				states[x][y-1] = gui.Miss
 			}
-		} else {
-			states[x][y-1] = gui.Miss
+			board.SetStates(states)
 		}
-		board.SetStates(states)
+		coordsChecked += shotsCounter
 	}
 
 	go func() {
 		for {
-			Shot(board, board2, &states, &states2, ui, client, token, &myHitCounter)
-			if myHitCounter == 20 {
-				txt.SetText(fmt.Sprintf("YOU WON: %s", data.Nick))
+			time.Sleep(time.Millisecond * 300)
+			data, err := GetGameStatus(client, token)
+			if err != nil {
+				ui.Log("cannot get data: %w", err)
 				break
 			}
-			Get_Opp_Shot(board, board2, &states, &states2, ui, client, token, &oppHitCounter)
-			if oppHitCounter == 20 {
-				txt.SetText(fmt.Sprintf("OPPONENT WON: %s", data.Opponent))
-				break
+
+			if data.Game_status == "ended" {
+				if data.Last_game_status == "win" {
+					txt.SetText(fmt.Sprintf("YOU WON"))
+					break
+				} else {
+					txt.SetText(fmt.Sprintf("OPPONENT WON"))
+					break
+				}
+			}
+
+			if data.Should_fire {
+				Shot(board, board2, &states, &states2, ui, client, token, &myHitCounter)
+			} else {
+				Get_Opp_Shot(board, board2, &states, &states2, ui, client, token, &oppHitCounter, &coordsChecked)
 			}
 		}
 	}()
 	ui.Start(nil)
-	// Get_Opp_Shot(board, board2, &states, &states2, ui, client, token)
-
-	// Shot(board, board2, &states, &states2, ui, client, token)
-	// go func() {
-	// 	for {
-	// 		char := board2.Listen(context.TODO())
-	// 		txt.SetText(fmt.Sprintf("Coordinate: %s", char))
-	// 		ui.Log("Coordinate: %s", char) // logs are displayed after the game exits
-	// 		x, y := ChangeCooerdinate(char)
-	// 		states2[x][y-1] = gui.Hit
-	// 		board2.SetStates(states2)
-	// 	}
-	// }()
-
-	// ui.Start(nil)
-
-	// bb := gui.New(
-	// 	gui.ConfigParams().
-	// 		HitChar('#').
-	// 		HitColor(color.FgRed).
-	// 		BorderColor(color.BgRed).
-	// 		RulerTextColor(color.BgYellow).
-	// 		NewConfig())
-
-	// bb.Import(bo.Board)
-	// bb.Display()
-
-	// fmt.Print(data.Nick + " vs " + data.Opponent)
 
 	return nil
 }
@@ -239,7 +240,7 @@ func ChangeCooerdinate(coordinate string) (int, int) {
 	return x, y
 }
 
-func Shot(myBoard, oppBoard *gui.Board, myStates, oppStates *[10][10]gui.State, ui *gui.GUI, client *http.Client, token string, myHitCounter *int) {
+func Shot(myBoard, oppBoard *gui.Board, myStates, oppStates *[10][10]gui.State, ui *gui.GUI, client *http.Client, token string, myHitCounter *int) error {
 	var x, y int
 	char := oppBoard.Listen(context.TODO())
 	ui.Log("My Shot: %s", char)
@@ -251,10 +252,12 @@ func Shot(myBoard, oppBoard *gui.Board, myStates, oppStates *[10][10]gui.State, 
 	req.Header.Add("x-auth-token", token)
 	if err != nil {
 		ui.Log("cannot send data: %w", err)
+		return err
 	}
 	resp, err := client.Do(req)
 	if err != nil {
 		ui.Log("cannot send data: %w", err)
+		return err
 	}
 
 	var data Fire_result
@@ -262,6 +265,7 @@ func Shot(myBoard, oppBoard *gui.Board, myStates, oppStates *[10][10]gui.State, 
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
 		ui.Log("cannot unmarshal data: %w", err)
+		return err
 	}
 	ui.Log("result: %s", data.Result)
 	if data.Result == "miss" {
@@ -271,43 +275,74 @@ func Shot(myBoard, oppBoard *gui.Board, myStates, oppStates *[10][10]gui.State, 
 		oppStates[x][y-1] = gui.Hit
 		*myHitCounter++
 		if *myHitCounter == 20 {
-			return
+			return nil
 		}
 		oppBoard.SetStates(*oppStates)
-		Shot(myBoard, oppBoard, myStates, oppStates, ui, client, token, myHitCounter)
 	}
+	return nil
 }
 
-func Get_Opp_Shot(myBoard, oppBoard *gui.Board, myStates, oppStates *[10][10]gui.State, ui *gui.GUI, client *http.Client, token string, oppHitCounter *int) {
-	var data GameStatus
-	// for len(data.Opp_shots) != *oppHitCounter+1 {
-	for len(data.Opp_shots) == 0 {
-		time.Sleep(time.Millisecond * 200)
-		req, err := http.NewRequest(http.MethodGet, "https://go-pjatk-server.fly.dev/api/game", nil)
-		req.Header.Set("x-auth-token", token)
-		resp, err2 := client.Do(req)
-		if err != nil || err2 != nil {
-			ui.Log("cannot create request: %w", err)
-		}
-
-		err3 := json.NewDecoder(resp.Body).Decode(&data)
-		if err3 != nil {
-			ui.Log("cannot unmarshal data: %w", err)
-		}
+func Get_Opp_Shot(myBoard, oppBoard *gui.Board, myStates, oppStates *[10][10]gui.State, ui *gui.GUI, client *http.Client, token string, oppHitCounter, coordsChecked *int) error {
+	time.Sleep(time.Millisecond * 500)
+	data, err := GetGameStatus(client, token)
+	if err != nil {
+		ui.Log("cannot get data: %w", err)
+		return err
 	}
 
 	ui.Log("DATA: %s", data)
-	x, y := ChangeCooerdinate(data.Opp_shots[len(data.Opp_shots)-1])
-	if myStates[x][y-1] == gui.Ship {
-		myStates[x][y-1] = gui.Hit
-		*oppHitCounter++
-		if *oppHitCounter == 20 {
-			return
-		}
-		time.Sleep(time.Second * 1)
-		Get_Opp_Shot(myBoard, oppBoard, myStates, oppStates, ui, client, token, oppHitCounter)
-	} else {
-		myStates[x][y-1] = gui.Miss
+	shotsCounter := 0
+	if *coordsChecked == len(data.Opp_shots) {
+		time.Sleep(time.Millisecond * 200)
+		Get_Opp_Shot(myBoard, oppBoard, myStates, oppStates, ui, client, token, oppHitCounter, coordsChecked)
 	}
-	myBoard.SetStates(*myStates)
+	for i := *coordsChecked; i < len(data.Opp_shots); i++ {
+
+		x, y := ChangeCooerdinate(data.Opp_shots[i])
+		shotsCounter++
+		if myStates[x][y-1] == gui.Ship {
+			myStates[x][y-1] = gui.Hit
+			*oppHitCounter++
+			if *oppHitCounter == 20 {
+				return nil
+			}
+		} else {
+			myStates[x][y-1] = gui.Miss
+		}
+		myBoard.SetStates(*myStates)
+	}
+	*coordsChecked += shotsCounter
+	return nil
+}
+
+func GetGameStatus(client *http.Client, token string) (GameStatus, error) {
+	var data GameStatus
+	req, err := http.NewRequest(http.MethodGet, "https://go-pjatk-server.fly.dev/api/game", nil)
+	req.Header.Set("x-auth-token", token)
+	resp, err2 := client.Do(req)
+	if err != nil || err2 != nil {
+		if err != nil {
+			return GameStatus{}, err
+		}
+		if err2 != nil {
+			return GameStatus{}, err2
+		}
+	}
+
+	err3 := json.NewDecoder(resp.Body).Decode(&data)
+	if err3 != nil {
+		return GameStatus{}, err3
+	}
+	return data, nil
+}
+
+func ShortenDesc(desc string) string {
+	ret := ""
+	for i, l := range strings.Split(desc, "") {
+		if i%21 == 0 {
+			ret += "\t"
+		}
+		ret += l
+	}
+	return ret
 }
